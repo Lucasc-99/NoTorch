b1175b18a3147b571b1a145067b04a5a2c719301
Lucas Cecchi <lucascecchi@gmail.com>
2022-03-03 12:29:12 -0500

    Add Dense layer

--- NoTorch/nn.py
+++ NoTorch/nn.py
@@ -1,11 +1,9 @@
-from typing import Callable
 from NoTorch.tensor import Tensor
 
 import numpy as np
 
 
 class Module:
-
     def parameters(self):
         return []
 
@@ -13,27 +11,19 @@ class Module:
         for p in self.parameters():
             p.grad = np.zeros_like(p.grad)
 
+
 class Dense(Module):
     """
-    Fully Connected Layer
+    Fully Connected Layer with sigmoid activation
     """
 
-    def __init__(self, in_neurons: int, out_neurons: int, activation: Callable):
-        self.weight = Tensor(np.zeros(in_neurons), ())
-        self.bias = Tensor(np.zeros(out_neurons), ())
-        self.activation = activation
-    
-    def __call__(self, x: Tensor):
-        pass
+    def __init__(self, in_neurons: int, out_neurons: int):
+        self.weight = Tensor(np.zeros(shape=[1, in_neurons, out_neurons]), ())
+        self.bias = Tensor(np.zeros(shape=[1, out_neurons]), ())
 
+    def __call__(self, x: Tensor):
+        
+        return x.sigmoid()
 
     def parameters(self):
         return [self.weight, self.bias]
-
-class ReLU(Module):
-    """
-    Rectified Linear Unit (ReLU) activation function
-    """
-
-    def __call__(self, x: Tensor):
-        pass
