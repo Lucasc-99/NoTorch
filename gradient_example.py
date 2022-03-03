b7846bf4121e1062f2db8d5ec46644f0819be424
Lucas Cecchi <lucascecchi@gmail.com>
2022-03-03 13:23:20 -0500

    Add grad and mlp example files

--- gradient_example.py
+++ gradient_example.py
@@ -1,18 +1,1 @@
-from NoTorch.tensor import Tensor
 
-a = Tensor(-4.0)
-b = Tensor(3.0)
-c = a + b
-d = a * b + b**3
-c += c + 1
-c += 1 + c + (-a)
-d += d * 2 + (b + a).relu()
-d += 3 * d + (b - a).relu()
-e = c - d
-f = e**2
-g = f / 2.0
-g += 10.0 / f
-print(f'{g.data:.4f}') # prints 24.7041, the outcome of this forward pass
-g.backward()
-print(f'{a.grad:.4f}') # prints 138.8338, i.e. the numerical value of dg/da
-print(f'{b.grad:.4f}') # prints 645.5773, i.e. the numerical value of dg/db
