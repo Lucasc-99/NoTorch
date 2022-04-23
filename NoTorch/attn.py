5919ac36b75a3d93170cf21180fd9f99f043eaaa
Lucasc-99 <lucascecchi@gmail.com>
2022-04-23 17:14:37 -0400

    First version multihead self attention in attn.py

--- NoTorch/attn.py
+++ NoTorch/attn.py
@@ -15,26 +15,44 @@ class MultiHeadAttention(Module):
     """
 
     def __init__(self, input_dim: int, heads: int):
-
         def _w_init() -> Tensor:
-            return Tensor(np.array(
-                [
-                    np.longdouble(np.random.randn(input_dim))
-                    * math.sqrt(2.0 / input_dim)
-                    for _ in range(input_dim)
-                ]
-            ))
-        
+            return Tensor(
+                np.array(
+                    [
+                        np.longdouble(np.random.randn(input_dim))
+                        * math.sqrt(2.0 / input_dim)
+                        for _ in range(input_dim)
+                    ]
+                )
+            )
+
         self.w_query = [_w_init() for _ in heads]
         self.w_key = [_w_init() for _ in heads]
         self.w_value = [_w_init() for _ in heads]
 
         self.heads = heads
         self.input_dim = input_dim
-        
-    def __call__(self, x: List[Union[Tensor, np.ndarray]]) -> List[Tensor]:
-        # TODO
-        pass
+
+    def __call__(self, x: List[Union[Tensor, np.ndarray]]) -> List[List[Tensor]]:
+        def y_i_r(i: int, r: int) -> Tensor:
+            """
+            Get vector output for token i, for head r
+            """
+
+            query_key_dot = [
+                (
+                    Tensor.dot(self.w_query[r], x[i])
+                    * Tensor.dot(self.w_key[r], x[j])
+                    / math.sqrt(len(x))
+                ).exp()
+                for j in range(len(x))
+            ]
+            qk_sum = sum(query_key_dot)
+            return sum([qkd / qk_sum for qkd in query_key_dot]) * Tensor.dot(
+                self.w_value[r], x[i]
+            )
+
+        return [[y_i_r(i, r) for i in range(len(x))] for r in range(self.heads)]
 
     def parameters(self):
         return [self.w_query, self.w_key, self.w_value]
